4cd71ed423fa91c2518f004528a6dd74f83110f7
idolevitt <idolevit2@gmail.com>
2021-01-11 21:39:17 +0200

    maze example
    adding another maze example to Main.java

--- src/mazer/Main.java
+++ src/mazer/Main.java
@@ -16,10 +16,19 @@ public class Main {
         arr[2][3] = -1;
         arr[2][2] = -1;
         arr[3][2] = -1;
+<<<<<<< Updated upstream
         maze.setMaze(arr);
 
         maze.setSource(1,1);
         maze.setTarget(4,4);
+=======
+        arr[3][1] = -1;
+        arr[1][3] = -1;
+        maze.setMaze(arr);
+
+        maze.setSource(1,1);
+        maze.setTarget(3,3);
+>>>>>>> Stashed changes
 
         System.out.println(maze.target);
 
@@ -29,6 +38,38 @@ public class Main {
 
         System.out.println(maze.target);
 
+<<<<<<< Updated upstream
+=======
+        Maze hardPathExample = new Maze(5);
+
+        // s - source, t - target , * - block
+        // 0  0  0  1  t
+        // 0  1  0  1  0
+        // 0  1  0  1  0
+        // 0  1  0  1  0
+        // s  1  0  0  0
+
+        int[][] mazeArray = new int[5][5];
+        for (int i = 0; i < 4; i++) {
+            mazeArray[i + 1][1] = -1;
+            mazeArray[i][3] = -1;
+        }
+        hardPathExample.setMaze(mazeArray);
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                System.out.print(mazeArray[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        hardPathExample.setSource(4,0);
+        hardPathExample.setTarget(0,4);
+        hardPathExample.printMaze();
+
+        hardPathExample.solveMaze();
+
+>>>>>>> Stashed changes
     }
 
 
